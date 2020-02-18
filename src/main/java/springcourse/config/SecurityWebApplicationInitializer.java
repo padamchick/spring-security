3f5b514340c9820deb1c1c3328a108038c2b2ccd
Piotrek <piotrek.adamczykk@gmail.com>
2020-02-18 09:14:23 +0100

    Initialize basic web app

---
+++ src/main/java/springcourse/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package springcourse.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
---
+++ src/main/java/springcourse/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package springcourse.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        // add our user for in memory authentication
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(users.username("john").password("test123").roles("EMPLOYEE"))
+                .withUser(users.username("mary").password("test123").roles("MANAGER"))
+                .withUser(users.username("susan").password("test123").roles("ADMIN"));
+    }
+}
